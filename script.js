47b29fa3166c0272805364826c68fbbf18ac968d
burrezo <burrezo@gmail.com>
2022-04-14 16:22:03 +0200

    Rock, paper, scissors project... refactored with UI!

--- script.js
+++ script.js
@@ -1,96 +1,125 @@
+const GAME_RULES = {
+    ROCK: {
+        SCISSORS: true,
+        LIZARD: true
+    },
+    PAPER: {
+        ROCK: true,
+        SPOCK: true
+    },
+    SCISSORS: {
+        PAPER: true,
+        LIZARD: true
+    },
+    LIZARD: {
+        SPOCK: true,
+        PAPER: true
+    },
+    SPOCK: {
+        SCISSORS: true,
+        ROCK: true
+    }
+}
+
+function computerPlay(btn) {
+    let randNum = Math.random();
+    if (randNum < 0.2) {
+        btn[0].classList.add('computerOption');
+        setTimeout(() => {
+           btn[0].classList.remove('computerOption');
+        }, 500);
+        return "ROCK";
+    }
+    else if (randNum < 0.4) {
+        btn[1].classList.add('computerOption');
+        setTimeout(() => {
+           btn[1].classList.remove('computerOption');
+        }, 500);
+        return "PAPER";
+    }
+    else if (randNum < 0.6) {
+        btn[2].classList.add('computerOption');
+        setTimeout(() => {
+           btn[2].classList.remove('computerOption');
+        }, 500);
+        return "SCISSORS";
+    }
+    else if (randNum < 0.8) {
+        btn[3].classList.add('computerOption');
+        setTimeout(() => {
+           btn[3].classList.remove('computerOption');
+        }, 500);
+        return "LIZARD";
+    }
+    else {
+        btn[4].classList.add('computerOption');
+        setTimeout(() => {
+           btn[4].classList.remove('computerOption');
+        }, 500);
+        return "SPOCK";
+    }
+}
+
+function playRound(user, computer, bazinga) {
+    
+    if (user === computer) {
+        tie.currentTime = 0;
+        tie.play();
+        return 'Tie!';
+    }
+    else if (GAME_RULES[user][computer]) {
+        userTotal++;
+        roundwin.currentTime = 0;
+        roundwin.play();
+        return 'You Win!';
+    }
+    else {
+        computerTotal++;
+        bazinga.currentTime = 0;
+        bazinga.play();
+        return 'You Loose';
+    }
+}
+
 let userTotal = 0;
 let computerTotal = 0;
-let round = 1;
 
-function computerPlay() {
-    let randNum = Math.random();
-    if (randNum < 0.333) return "Rock";
-    else if (randNum < 0.666) return "Paper";
-    else return "Scissors";
-}
+const btn = document.querySelectorAll("[id^='btn-']");
 
-function userPlay() {
+const bazinga = document.querySelector('#bazinga');
+const roundwin = document.querySelector('#roundwin');
+const tie = document.querySelector('#tie');
+const youwin = document.querySelector('#youwin');
+const youlose = document.querySelector('#youlose');
 
-    let playerSelectionLowerCase;
+const div = document.querySelector('#score');
+const p = document.createElement('p');
+const h3 = document.createElement('h3');
+
+div.appendChild(p);
+div.appendChild(h3);
+
+btn.forEach(button => button.addEventListener('click', () => {
+
+    h3.textContent = playRound(button.alt, computerPlay(btn), bazinga); 
     
-    do {
+    button.classList.add('optionClick');
+    setTimeout(() => {
+       button.classList.remove('optionClick');
+    }, 100);
 
-    let playerSelection = prompt(`Round ${round}/5\n\nUser: ${userTotal} Computer: ${computerTotal}\n\nChoose rock, paper or scissors:`);
-
-    playerSelectionLowerCase = playerSelection.toLowerCase();
-
-    if (playerSelectionLowerCase != "rock" && playerSelectionLowerCase != "paper" && playerSelectionLowerCase != "scissors") alert("Don't understand, repeat please.");
-
-    } while (playerSelectionLowerCase != "rock" && playerSelectionLowerCase != "paper" && playerSelectionLowerCase != "scissors");
-
-    let playerSelectionCapitalized = playerSelectionLowerCase.charAt(0).toUpperCase() + playerSelectionLowerCase.slice(1);
-
-    return playerSelectionCapitalized;
-}
-
-function playRound(user, computer) {
-
-    let computerRock = "Computer choose Rock, ";
-    let computerPaper = "Computer choose Paper, ";
-    let computerScissors = "Computer choose Schissors, ";
-
-    let paperRock = "Paper wins Rock";
-    let rockScissors = "Rock wins Scissors";
-    let scissorsPaper = "Scissors wins Paper";
-    let rockRock = "Tie!";
-    let paperPaper = "Tie!";
-    let scissorsScissors = "Tie!";
-
-    let youLose = " , you lose!";
-    let youWin = " , you win!";
-
-    if (user == "Rock") {
-        if (computer == "Paper") { 
-            alert(computerPaper + paperRock + youLose);
-            computerTotal++;
-        }
-        else if (computer == "Scissors") {
-            alert(computerScissors + rockScissors + youWin);
-            userTotal++;
-        } 
-        else {
-            alert(computerRock + rockRock);
-        }
+    p.textContent = `User: ${userTotal} Vs Computer: ${computerTotal}`;
+    
+    if (userTotal >= 5) {
+        h3.textContent = 'YOU WIN!!';
+        youwin.play();
+        if (confirm('Do you wanna play again?')) location.reload();
+        else localtion.assign('https://www.theodinproject.com')
     }
-    else if (user == "Paper") {
-        if (computer == "Rock") { 
-            alert(computerRock + paperRock + youWin);
-            userTotal++;
-        }
-        else if (computer == "Scissors") {
-            alert(computerScissors + scissorsPaper + youLose);
-            computerTotal++;
-        } 
-        else {
-            alert(computerPaper + paperPaper);
-        }
-    }
-    else if (user == "Scissors") {
-        if (computer == "Paper") { 
-            alert(computerPaper + scissorsPaper + youWin);
-            userTotal++;
-        }
-        else if (computer == "Rock") {
-            alert(computerRock + rockScissors + youLose);
-            computerTotal++;
-        } 
-        else {
-            alert(computerScissors + scissorsScissors);
-        }
-    }
-}
-
-for (round = 1; round < 6; round++) {
-    playRound(userPlay(), computerPlay());
-}
-
-let finalAlert;
-if (userTotal > computerTotal) finalAlert = "You WIN!!!";
-else if (userTotal < computerTotal) finalAlert = "You LOSE!!!";
-else finalAlert = "TIE!"
-alert(`Final score:\n\nUser: ${userTotal} Computer: ${computerTotal}\n\n${finalAlert}`)
+    else if (computerTotal >= 5) {
+        h3.textContent = 'YOU LOOSE!';
+        youlose.play();
+        if (confirm('Do you wanna play again?')) location.reload();
+        else localtion.assign('https://www.theodinproject.com')
+    } 
+}));
